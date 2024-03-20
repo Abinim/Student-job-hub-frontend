240a4abc0e3db8a1fdeacb10f623b42718aac63a
bibek <abinim.bibek19@gmail.com>
2024-03-20 21:35:57 +0000

    Home updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,80 @@
-import React, { useState, useEffect } from 'react';
-import { Grid, Text, Button, Box } from '@chakra-ui/react';
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { Box, Flex, Heading, Text, Button } from '@chakra-ui/react';
 
-const Home = () => {
-  const [jobs, setJobs] = useState([]);
-
-  useEffect(() => {
-    const fetchJobs = async () => {
-      try {
-        const response = await fetch('/api/jobs');
-        if (!response.ok) {
-          throw new Error(`Failed to fetch jobs. Status: ${response.status}`);
-        }
-        const data = await response.json();
-        console.log('Response data:', data);
-        setJobs(data);
-      } catch (error) {
-        console.error('Error fetching jobs:', error.message);
-      }
-    };
-
-    fetchJobs();
-  }, []);
-
+function Home() {
   return (
-    <Grid templateColumns='1fr' gap={5} p={100}>
-      {Array.isArray(jobs) && jobs.length > 0 ? (
-        jobs.map(job => (
-          <Box key={job._id} p={4} borderWidth='1px' borderRadius='md' mb={4}>
-            <Text fontSize='xl'>{job.companyName}</Text>
-            <Text fontSize='md' mt={2}>
-              {job.address}
-            </Text>
-            <Text mt={2}>{job.description}</Text>
-          </Box>
-        ))
-      ) : (
-        <Text>Welcome to the JobHunt zone</Text>
-      )}
-    </Grid>
+    <Box
+      p='6'
+      bg='transparent'
+      margin={'150px'}
+      borderRadius='50px'
+      minHeight='100vh'
+    >
+      <Flex direction='column' alignItems='center'>
+        <Box
+          className='box-animation'
+          bg='#ef476f'
+          p='40'
+          borderRadius='20px'
+          boxShadow='lg'
+          mb='6'
+        >
+          <Heading as='h1' size='xl' textAlign='center' color='white'>
+            Welcome to Our Student Jobs Portal
+          </Heading>
+          <Text fontSize='lg' textAlign='center' color='white'>
+            We provide short-term and part-time job opportunities tailored for
+            students.
+          </Text>
+        </Box>
+        <Box
+          className='box-animation'
+          bg='#ffd166'
+          p='30'
+          margin={'50px'}
+          borderRadius='20px'
+          boxShadow='lg'
+          mb='6'
+        >
+          <Heading as='h2' size='lg' color='white' mb='4'>
+            Available Jobs
+          </Heading>
+          <Text color='white' fontSize='lg' mb='4'>
+            Explore our latest job opportunities and kickstart your career!
+          </Text>
+          <Button
+            as={RouterLink}
+            to='/all-jobs'
+            colorScheme='green'
+            size='lg'
+            mx='auto'
+            display='block'
+            color={'red'}
+          >
+            View Available Jobs
+          </Button>
+        </Box>
+        <Box
+          className='box-animation'
+          bg='#06d6a0'
+          p='20'
+          margin={'50px'}
+          borderRadius='20px'
+          boxShadow='lg'
+        >
+          <Heading as='h2' size='lg' color='white' mb='4'>
+            Our Projects
+          </Heading>
+          <Text color='white'>
+            Here you can describe your projects or any relevant information
+            about your website. You can add images, links, or further details to
+            showcase your projects.
+          </Text>
+        </Box>
+      </Flex>
+    </Box>
   );
-};
+}
 
 export default Home;
